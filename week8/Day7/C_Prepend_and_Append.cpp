762025948bac06e002574135f746fe59a673067d
Mahirhasan25 <mahinhasan.ar@gmail.com>
2023-08-03 21:35:25 +0600

    week 8, day 7

---
+++ week8/Day7/C_Prepend_and_Append.cpp
@@ -0,0 +1,38 @@
+/*
+       || Mahir Hasan ||
+*/
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define vpair vector<pair<ll, ll>>
+#define PI 3.14159265359
+#define cyes cout << "YES" << '\n'
+#define cno cout << "NO" << '\n'
+#define print(x) cout << x << '\n'
+#define minus cout << "-1" << '\n'
+#define pub push_back
+#define pob pop_back
+#define rep(x, y) for(int i = x; i < y; i++)
+#define all(x) (x).begin(),(x).end()
+#define fast ios_base::sync_with_stdio(false);\
+    cin.tie(NULL);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+    int i = 0, j = n-1;
+    while(i <= j && s[i] != s[j]){
+        n -= 2;
+        i++; j--;
+    }
+    print(n);
+}
+int main(){
+    fast
+    int t; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
---
+++ week8/Day7/E_Negatives_and_Positives.cpp
@@ -0,0 +1,42 @@
+/*
+       || Mahir Hasan ||
+*/
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define vpair vector<pair<ll, ll>>
+#define PI 3.14159265359
+#define cyes cout << "YES" << '\n'
+#define cno cout << "NO" << '\n'
+#define print(x) cout << x << '\n'
+#define minus cout << "-1" << '\n'
+#define pub push_back
+#define pob pop_back
+#define rep(x, y) for(int i = x; i < y; i++)
+#define all(x) (x).begin(),(x).end()
+#define fast ios_base::sync_with_stdio(false);\
+    cin.tie(NULL);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    ll sum = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] <= 0 && a[i+1] <= 0 && i+1 < n){
+            a[i] *= -1;
+            a[i+1] *= -1;
+        }
+        sum += a[i];
+    }
+    print(sum);
+}
+int main(){
+    fast
+    int t; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
